062658f1a7d635b0bd91e635e4e69032d1b90692
Mohamed Assem <mohamed.assem.ali@gmail.com>
2024-06-30 04:53:59 +0300

    Implement Splash Screen

--- app/src/main/java/com/assem/albumsapp/MainActivity.kt
+++ app/src/main/java/com/assem/albumsapp/MainActivity.kt
@@ -3,6 +3,7 @@ package com.assem.albumsapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
@@ -17,6 +18,7 @@ import dagger.hilt.android.AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
         setContent {
             AlbumsAppTheme {
                 val navController = rememberNavController()
--- app/src/main/java/com/assem/albumsapp/presentation/albums_list/AlbumsListScreen.kt
+++ app/src/main/java/com/assem/albumsapp/presentation/albums_list/AlbumsListScreen.kt
@@ -1,22 +1,14 @@
 package com.assem.albumsapp.presentation.albums_list
 
-import android.widget.Toast
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
-import androidx.compose.material3.Divider
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
-import androidx.navigation.compose.rememberNavController
 import com.google.accompanist.swiperefresh.SwipeRefresh
 import com.google.accompanist.swiperefresh.rememberSwipeRefreshState
 
@@ -33,7 +25,6 @@ fun AlbumsListScreen(
         isRefreshing = viewModel.state.isRefreshing
     )
     val state = viewModel.state
-
     Column(
         modifier = Modifier.fillMaxSize()
     ) {
